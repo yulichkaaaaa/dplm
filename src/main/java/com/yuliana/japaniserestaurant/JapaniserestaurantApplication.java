6cfb7bac5c938ffea34e74a0674549cfc708b434
Юля <yulia.shuleiko@gmail.com>
2022-06-20 20:51:02 +0300

    create project

---
+++ src/main/java/com/yuliana/japaniserestaurant/JapaniserestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.yuliana.japaniserestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaniserestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapaniserestaurantApplication.class, args);
+    }
+
+}
